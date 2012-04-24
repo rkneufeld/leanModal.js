160a02155a7cc2109c8fb349e0355a35190f45af
Chad Humphries and Ryan Neufeld <pair+ch+rn@thinkrelevance.com>
2012-04-24 15:10:41 -0500

    JSHint fixes, whitespace cleanup, and consistent space/tab usage

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,60 @@
 (function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
-            }
-            
-            var overlay = $("<div id='lean_overlay'></div>");
-            
-            $("body").append(overlay);
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-              	var modal_id = $(this).attr("href");
+  $.fn.extend({
+    leanModal: function(options) {
 
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                
-                $(o.closeButton).click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+      var close_modal = function (modal_id){
+        $("#lean_overlay").fadeOut(200);
+        $(modal_id).css({ 'display' : 'none' });
+      };
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+      var defaults = {
+        top: 100,
+        overlay: 0.5,
+        closeButton: null
+      };
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
+      var overlay = $("<div id='lean_overlay'></div>");
 
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
+      $("body").append(overlay);
 
-        		$(modal_id).fadeTo(200,1);
+      options = $.extend(defaults, options);
 
-                e.preventDefault();
-                		
-              	});
-             
-            });
+      return this.each(function() {
+        var o = options;
 
-			function close_modal(modal_id){
+        $(this).click(function(e) {
+          var modal_id = $(this).attr("href");
 
-        		$("#lean_overlay").fadeOut(200);
+          $("#lean_overlay").click(function() {
+            close_modal(modal_id);
+          });
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
-        }
-    });
-     
+          $(options.closeButton).click(function() {
+            close_modal(modal_id);
+          });
+
+          var modal_height = $(modal_id).outerHeight();
+          var modal_width = $(modal_id).outerWidth();
+
+          var lean_overlay = $('#lean_overlay');
+          lean_overlay.css({ 'display' : 'block', opacity : 0 });
+          lean_overlay.fadeTo(200, o.overlay);
+
+          var modal = $(modal_id);
+          modal.css({
+            'display' : 'block',
+            'position' : 'fixed',
+            'opacity' : 0,
+            'z-index': 11000,
+            'left' : 50 + '%',
+            'margin-left' : -(modal_width/2) + "px",
+            'top' : o.top + "px"
+          });
+
+          modal.fadeTo(200,1);
+          e.preventDefault();
+        }); // $(this).click(function(e) ...
+      }); // return this.each(function() ...
+    } // leanModal: function(options) ...
+  }); // $.fn.extend( ...
 })(jQuery);
